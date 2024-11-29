9b3e2c06b8a91f1d66bb7ceeb73a0fbec451b9a1
noorul <noorul.khan@zettalabs.com>
2024-11-30 03:25:45 +0530

    30Nov24: 446.  Find All Numbers Disappeared in an Array: (kind of Missing Number) lc -easy type on Cycle sort ALGO

--- CoreJava/src/kunalSeries/sortingalgo/_4_cyclicsort/practice/_2_FindiAllNumDissappearedInAnArray.java
+++ CoreJava/src/kunalSeries/sortingalgo/_4_cyclicsort/practice/_2_FindiAllNumDissappearedInAnArray.java
@@ -78,31 +78,15 @@ public class _2_FindiAllNumDissappearedInAnArray {
         System.out.println("Missing number from above Array: " + missingNumberLst);
     }
 }
-// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 /*
-Constraints:
-
-        n == nums.length
-        1 <= n <= 10^4
-        0 <= nums[i] <= n
-        All the numbers of nums are unique.
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 
 Example 1:
 
-Input: nums = [3,0,1]
-Output: 2
-Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
-
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
 Example 2:
 
-Input: nums = [0,1]
-Output: 2
-Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
-
-Example 3:
-
-Input: nums = [9,6,4,2,3,5,7,0,1]
-Output: 8
-Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
-
+Input: nums = [1,1]
+Output: [2]
  */
