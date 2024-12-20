02614c9df70642f35ed000c2c371da236b30c597
noorul <noorul.khan@zettalabs.com>
2024-12-20 23:01:28 +0530

    20dec24: Maths - bitwise operators stuff

---
+++ CoreJava/src/kunalSeries/basics/concepts/maths/bitwiseoperator/practice/_1_OddEvenCheck.java
@@ -0,0 +1,9 @@
+package kunalSeries.basics.concepts.maths.bitwiseoperator.practice;
+
+// TODO (ts-00:33:00) : https://www.youtube.com/watch?v=fzip9Aml6og&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=29
+// why we & with the 1 to the given number and check odd or even - explained
+public class _1_OddEvenCheck {
+    public static void main(String[] args) {
+
+    }
+}
